72c6254d8dae50477ea0680c8eca242c49eb5cb8
Aleks Tsilyurik <ts.alex85@gmail.com>
2017-08-21 16:00:46 +0300

    include bootstrap 4, fa icons and made styles for app.component, dashboard, hero-search, heroes

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,17 +3,17 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-        { id: 0, name: 'Zero' },
-        { id: 11, name: 'Mr. Nice' },
-        { id: 12, name: 'Narco' },
-        { id: 13, name: 'Bombasto' },
-        { id: 14, name: 'Celeritas' },
-        { id: 15, name: 'Magneta' },
-        { id: 16, name: 'RubberMan' },
-        { id: 17, name: 'Dynama' },
-        { id: 18, name: 'Dr. IQ' },
-        { id: 19, name: 'Magma' },
-        { id: 20, name: 'Tornado' }
+        { id: 0, name: 'Zero', img: 'http://lorempixel.com/400/200/people/1' },
+        { id: 11, name: 'Mr. Nice', img: 'http://lorempixel.com/400/200/people/2'  },
+        { id: 12, name: 'Narco', img: 'http://lorempixel.com/400/200/people/3'  },
+        { id: 13, name: 'Bombasto', img: 'http://lorempixel.com/400/200/people/4'  },
+        { id: 14, name: 'Celeritas', img: 'http://lorempixel.com/400/200/people/5'  },
+        { id: 15, name: 'Magneta', img: 'http://lorempixel.com/400/200/people/6'  },
+        { id: 16, name: 'RubberMan', img: 'http://lorempixel.com/400/200/people/7'  },
+        { id: 17, name: 'Dynama', img: 'http://lorempixel.com/400/200/people/8'  },
+        { id: 18, name: 'Dr. IQ', img: 'http://lorempixel.com/400/200/people/9'  },
+        { id: 19, name: 'Magma', img: 'http://lorempixel.com/400/200/people/10'  },
+        { id: 20, name: 'Tornado', img: 'http://lorempixel.com/400/200/people/11'  }
     ];
     return {heroes};
   }
